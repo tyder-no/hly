6545b69e08a0fcd286c0c8236cd26d31442c35f3
t_a_ydersbond <t_a_ydersbond@yahoo.no>
2018-02-12 17:26:03 +0100

    File saved 2

--- src/test/java/no/ssb/health/hly/CompKernelTest.java
+++ src/test/java/no/ssb/health/hly/CompKernelTest.java
@@ -11,6 +11,7 @@ import static org.junit.Assert.assertTrue ;
 import static org.junit.Assert.assertFalse ;
 import static org.junit.Assert.assertEquals ;
 
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
